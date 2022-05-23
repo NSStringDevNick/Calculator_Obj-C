716830323bd60f760e93952202acc24a12979979
NSStringDevNick <dev.nik.98@gmail.com>
2022-05-23 13:33:26 +0300

    mistake

---
+++ Triangle/Triangle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Triangle
+//
+//  Created by Mikita  on 23/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
